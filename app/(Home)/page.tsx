124ddd3ee59825111a5bccfadb9900665716d260
amuhaimen <amuhaimenpro829@gmail.com>
2026-01-12 16:36:10 +0600

    navbar done

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function LandingPage() {
   return (
-    <div>
-        <div>
+    <div className=''>
+        <div className='container'>
             <AboutSoftvence/>
         </div>
     </div>
